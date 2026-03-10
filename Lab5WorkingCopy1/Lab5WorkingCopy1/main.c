e36b9be75dba224778daa47bdd9c446d65c55b60
WilFehr <125421373+WilFehr@users.noreply.github.com>
2026-03-09 17:20:45 -0700

    formatting main

--- Lab5WorkingCopy1/Lab5WorkingCopy1/main.c
+++ Lab5WorkingCopy1/Lab5WorkingCopy1/main.c
@@ -41,9 +41,9 @@ const char steps_arr[4] = {0b00110000, 0b00000110, 0b00101000, 0b00000101};
 
 /***********    START OF FUNCTION DECLARATIONS   ***************/
 void nTimer(int count);
-void home_stepper();
-void turnCW90();
-void turnCCW90();
+void home_stepper();//home in on black
+void turnCW90();//turn stepper
+void turnCCW90();//turn stepper
 /***********    END OF FUCTION DECLARATIONS   ***************/
 
 
@@ -56,27 +56,29 @@ void turnCCW90();
 
 int main(void)
 {
+
+/************* CLOCK SET UP ***************/
     CLKPR = 0x80;
     CLKPR = 0x01;		//  sets system clock to 8MHz
 	
-	/*
-	pre-scale TCCR1 to 1/8th
-	*/
+	
+	//pre-scale TCCR1 to 1/8th
 	//TCCR1,3,4, and 5 are 16 bit 
 	TCCR1B |= _BV(CS11); //bitwise or and assignment table 17-6
+/****** END CLOCK SET UP ***********/
 
-    STATE = 0;
 
     cli();		// Disables all interrupts
 
-	/******* PORT USAGE********/
+
+/***************** PORT USAGE*****************/
 	//PORTA
 	//STEPPER MOTOR
 	DDRA = 0xFF;
 	
 	//PORTB
 	//DCMOTOR
-	//DDRB = 0b10001111;
+	DDRB = 0b10001111;
 	
 	//PORTC
 	//DISPLAY ON PORT C
@@ -84,7 +86,7 @@ int main(void)
 	
 	//PORTD
 	//set bits 3-0 as inputs
-	//DDRD = 0xF0;
+	DDRD = 0xF0;
 	
 	//PORTE
 	
@@ -94,43 +96,61 @@ int main(void)
 	
 	//PORTL
 	//bit 7 is Hall effect
-	DDRL = 0b00000000;
+	DDRL = 0b00000000;	
+/****************** END PORT USAGE ************/
 	
-	/***** END PORT USAGE *****/
 	
-	
-	/****** *******/ 
-	/****** ******/
+/****** LCD INIT *******/ 
 	//Initialize LCD module
 	InitLCD(LS_BLINK|LS_ULINE);
 	LCDClear();
+/****** END LCD INIT ******/
+
+
+/************ INTERRUPT CONFIG ************/
+	// config the external interrupt ======================================
+	//table15-1
+	EIMSK |= (_BV(INT2)); // enable INT2
+	EICRA |= _BV(ISC21); // falling edge interrupt
+	EICRA &= ~_BV(ISC20); // falling edge interrupt
+
+	EIMSK |= (_BV(INT3)); // enable INT3
+	EICRA |= _BV(ISC31); // rising edge interrupt
+	EICRA |= _BV(ISC30); // rising edge interrupt
+/************ END INTERRUPT CONFIG ************/
 	
+/********** ADC CONFIG ***************/
+	// by default, the ADC input (analog input is set to be ADC0 / PORTF0
+	ADCSRA |= _BV(ADEN);		// enable ADC
+	ADCSRA |= _BV(ADIE);		// enable interrupt of ADC
+
+	//ADLAR- analog digital Left Adjust register
+	//REFS0- reference selection(turn on capacitor at AVCC
+	ADMUX |= _BV(ADLAR) | _BV(REFS0); // Read Technical Manual & Complete Comment
+
+/********** END ADC CONFIG ***************/
 	
-	home_stepper();
-	LCDWriteInt(curposition, 3);
-	nTimer(500);
-	LCDClear();
-	turnCW90();
+	sei();//global interrupt enable
+	
+ /****************           PWM INIT             **************************/
+	 //set up port B for fast PWM, fast pwm WGM0,WGM1 all high
+	 //com0a1, is clear OC0A on compare match, (table 16-2)
+	 TCCR0A |= 0b10000011;//sets both WGM01 and WGM00 to one also set COM0A1 to one,
+ 
+	 //Timer counter 2
+	 TCCR0B |= 0b00000011; //divide clock by 8(still too big) makes it 3.9kHz, 64 0b00000011
+ 
+	 //output compare register
+	 //8 bit count this is duty cycle
+	 OCR0A = 0x40;//64(25%)
+ /****************          END PWM INIT             **************************/
+	
+	//home_stepper();
+	
+	STATE = 0;
 
 	
-	LCDWriteInt(curposition, 3);
-	nTimer(500);
-	LCDClear();
-	turnCCW90();
-
 	
-	LCDWriteInt(curposition, 3);
-	nTimer(500);
-	LCDClear();
-	turnCW90();
-	
-	
-	LCDWriteInt(curposition, 3);
-	nTimer(500);
-	LCDClear();
-	turnCW90();
-	
-	LCDWriteInt(curposition, 3);
     while (1) 
     {
     }
